968f2eea3e2c4b5f219a4145be61acc08ea6bdf1
unknown <mary_baklanova@mail.ru>
2023-02-23 21:29:18 +0300

    last changes

--- WebServerPractice.py
+++ WebServerPractice.py
@@ -14,15 +14,13 @@ class PythonSimpleWebServer(BaseHTTPRequestHandler):
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        response = BytesIO
-        response.write('Post request')
-        response.write('Received: ')
-        response.write(body)
-        self.wfile.write(response.getvalue())
+
+        post_body = self.rfile.read(content_length)
+      #  self.wfile.write("received post request:<br>{}".format(post_body))
+        self.wfile.write(post_body)
 
 
 httpd = HTTPServer(('localhost', 8080), PythonSimpleWebServer)
